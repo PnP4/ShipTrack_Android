60b71e1971912f7b75f4b3ed4d1cfb1c4da3deb6
DLPDS <tcdlpds@gmail.com>
2016-08-08 00:42:40 +0530

    Start the repository, Add google map, Add alert window

---
+++ app/src/main/java/com/ucsc/pnp/shiptrack/StartActivity.java
@@ -0,0 +1,75 @@
+package com.ucsc.pnp.shiptrack;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class StartActivity extends AppCompatActivity {
+    ProgressDialog progDailog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        progDailog=ProgressDialog.show(this,"Wait", "Loading");
+        new InitLoader().execute();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_start, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_see_alert) {
+            startActivity(new Intent(StartActivity.this,AlertShow.class));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    class InitLoader extends AsyncTask<String,Void,String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                Thread.sleep(4000,0);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            progDailog.hide();
+        }
+    }
+
+}
